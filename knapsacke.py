7be9fbacb115a4fbfefdeba7c30bad6d7bb55093
mnemonico <b.mohamedachraf@gmail.com>
2018-09-19 12:43:33 +0200

    Update knapsacke.py

--- knapsacke.py
+++ knapsacke.py
@@ -2,7 +2,28 @@
 """
 Created on Wed Sept 18 21:50:29 2018
 @author: Achraf Baiz
-"""
+
+This algorithm try to search in the different levels of the builed tree to find the appropriate set of weigths that will be equal or closest to the bag weight
+without going greater than it;
+Our tree of sets combination is made bu using lists, eash element of it is a level of the tree, and each level is a number of combination sets.
+
+example 
+
+    - items   = {60, 100, 120}
+     respective
+    - weights = {10, 20, 30}
+     bag maximum size 
+    - w       =  50
+     
+                    {10 , 20 , 30}=60                                   LEVEL 1
+       --------------------------------------------
+     {10 , 20}=30         {10 , 30}=40        {20 , 30}=50              LEVEL 2
+       --------------------------------------------
+        {10}=10              {20}=20                {30}=30             LEVEL 3
+     
+     folowing the rules, the best set would be {20,30} = 50 (w = 50) which means the respective items {100,120} = 220  
+
+""" 
 
 def combination(list,n,r):
     tab    = []
